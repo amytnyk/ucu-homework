e347b9f256cb7aad1fff7ea567781119da6e7657
amytnyk <alex.mytnykg@gmail.com>
2021-11-17 01:56:26 +0200

    gitignore update

---
+++ lab7/calculus.py
@@ -0,0 +1,92 @@
+# Complete all of the following functions. Currently they all just
+# 'pass' rather than explicitly return value, which means that they
+# implicitly return None.
+
+def find_max_1(f, points):
+    """
+    (function or str, list(number)) -> (number)
+
+    Find and return maximal value of function f in points.
+
+    >>> find_max_1('x ** 2 + x', [1, 2, 3, -1])
+    12
+    >>> find_max_1(lambda x: x ** 2 + x, [1, 2, 3, -1])
+    12
+    """
+    if isinstance(f, str):
+        return max(map(lambda x: eval(f), points))
+    return max(map(lambda x: f(x), points))
+
+
+def find_max_2(f, points):
+    """
+    (function or str, list(number)) -> (number)
+
+    Find and return list of points where function f has the maximal value.
+
+    >>> find_max_2('x ** 2 + x', [1, 2, 3, -1])
+    [3]
+    >>> find_max_2(lambda x: x ** 2 + x, [1, 2, 3, -1])
+    [3]
+    """
+    pass
+
+
+def compute_limit(seq):
+    """
+    (function or str) -> (number)
+
+    Compute and return limit of a convergent sequence.
+
+    >>> compute_limit('(n ** 2 + n) / n ** 2')
+    1.0
+    >>> compute_limit(lambda n: (n ** 2 + n) / n ** 2)
+    1.0
+    """
+    pass
+
+
+def compute_derivative(f, x_0):
+    """
+    (function or str, number) -> (number)
+
+    Compute and return derivative of function f in the point x_0.
+
+    >>> compute_derivative('x ** 2 + x', 2)
+    5.0
+    >>> compute_derivative(lambda x: x ** 2 + x, 2)
+    5.0
+    """
+    pass
+
+
+def get_tangent(f, x_0):
+    """
+    (function or str, number) -> (str)
+
+    Compute and return tangent line to function f in the point x_0.
+
+    >>> get_tangent('x ** 2 + x', 2)
+    '5.0 * x - 4.0'
+    >>> get_tangent('- x ** 2 + x', 2)
+    '- 3.0 * x - 4.0'
+    >>> get_tangent(lambda x: x ** 2 + x, 2)
+    '5.0 * x - 4.0'
+    >>> get_tangent(lambda x: - x ** 2 + x, 2)
+    '- 3.0 * x - 4.0'
+    """
+    pass
+
+
+def get_root(f, a, b):
+    """
+    (function or str, number, number) -> (number)
+
+    Compute and return root of the function f in the interval (a, b).
+
+    >>> get_root('x', -1, 1)
+    0.0
+    >>> get_root(lambda x: x, -1, 1)
+    0.0
+    """
+    pass
